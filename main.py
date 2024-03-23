998187d156a82a8a618fd9f576f527d206112153
andreshrao <andreshrao@gmail.com>
2024-03-23 13:08:21 +0100

    creation base structure folders, env file and request import test

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from modules import SystemPythnotion
+
+#-----------------------------------------------------------
+def main():
+    pn = SystemPythnotion()
+    pn.run()
+
+
+#-----------------------------------------------------------
+if __name__ == "__main__":
+    os.system("clear")
+    main()
---
+++ modules/system.py
@@ -0,0 +1,27 @@
+import os
+import requests
+from datetime import datetime
+from dotenv import load_dotenv
+
+"""
+...
+"""
+
+
+#----------------------------------------------------------------------------------------------
+class SystemPythnotion():
+    """
+    ...
+    """
+
+    def __init__(self):
+        """ ... """
+        load_dotenv()
+        NOTION_TOKEN = "NOTION_TOKEN"
+        DATABASE_ID = "DATABASE_ID"
+        print(NOTION_TOKEN)
+        print(DATABASE_ID)
+
+    def run(self):
+        """ ... """
+        pass
